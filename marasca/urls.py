8b6636ebd1a06a155c5d062adc9fa0bbda6ec2d2
Jakub Wilk <jwilk@jwilk.net>
2012-03-21 18:02:27 +0100

    urls: reorder URL patterns.

--- marasca/urls.py
+++ marasca/urls.py
@@ -18,15 +18,18 @@ from app import views
 from utils import redirect
 
 urlpatterns = patterns('',
-    url(r'^$', views.process_index),
+    # technical stuff
     url(r'^ping/', views.process_ping),
-    url(r'^settings/$', views.process_settings, name='settings'),
+    url(r'^redirect/(?P<key>[A-Za-z0-9_-]+)/(?P<scheme>http)/(?P<tail>.*)$', redirect.safe_redirect),
+    url(r'^i18n/set-language/', views.set_language),
+    # media
     url(r'^css/(?P<path>.*)$', 'django.views.static.serve', dict(document_root='media/css/'), name='css'),
     url(r'^js/(?P<path>.*)$', 'django.views.static.serve', dict(document_root='media/js/'), name='js'),
     url(r'^extra/(?P<path>.*)$', 'django.views.static.serve', dict(document_root='media/extra/'), name='extra'),
+    # translatable content
+    url(r'^$', views.process_index),
+    url(r'^settings/$', views.process_settings, name='settings'),
     url(r'^help/$', views.process_cheatsheet, name='help'),
-    url(r'^redirect/(?P<key>[A-Za-z0-9_-]+)/(?P<scheme>http)/(?P<tail>.*)$', redirect.safe_redirect),
-    url(r'^i18n/set-language/', views.set_language),
     url(r'^(?P<corpus_id>[\w-]+)/$', views.corpus_info, name='corpus'),
     url(r'^(?P<corpus_id>[\w-]+)/query/(?P<page_start>[0-9]+)[+]/$', views.process_query, dict(query=True), name='query'),
     url(r'^(?P<corpus_id>[\w-]+)/query/(?P<nth>[0-9]+)/$', views.process_query, dict(query=True), name='query'),
