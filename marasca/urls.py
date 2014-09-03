5f18cbe70c26764c3c282d2c8e3176cfd3319783
Jakub Wilk <jwilk@jwilk.net>
2014-09-03 10:57:43 +0200

    url: fix compatiblity with Django 1.6.

--- marasca/urls.py
+++ marasca/urls.py
@@ -11,8 +11,24 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 # General Public License for more details.
 
-from django.conf.urls.defaults import *
-import django.views.generic.simple
+import django
+
+if django.VERSION < (1, 4):
+    from django.conf.urls.defaults import *
+    def template_view(template):
+        return [
+            'django.views.generic.simple.direct_to_template',
+            dict(template=template)
+        ]
+else:
+    # https://docs.djangoproject.com/en/1.6/releases/1.4/#django-conf-urls-defaults
+    # https://docs.djangoproject.com/en/1.6/internals/deprecation/#id1
+    from django.conf.urls import *
+    import django.views.generic.base
+    def template_view(template):
+        return [
+            django.views.generic.base.TemplateView.as_view(template_name=template),
+        ]
 
 from app import views
 from utils import redirect
@@ -35,8 +51,8 @@ urlpatterns = patterns('',
     url(r'^(?P<corpus_id>[\w-]+)/query/(?P<nth>[0-9]+)/$', views.process_query, dict(query=True), name='query'),
     url(r'^(?P<corpus_id>[\w-]+)/query/$', views.process_query, dict(query=True), name='query'),
     url(r'^(?P<corpus_id>[\w-]+)/query/(?:[0-9]+[+]?/)?m(?P<nth>[0-9]+)/$', views.process_metadata_snippet),
-    url(r'^error/404/', django.views.generic.simple.direct_to_template, dict(template='404.html')),
-    url(r'^error/500/', django.views.generic.simple.direct_to_template, dict(template='500.html')),
+    url(r'^error/404/', *template_view(template='404.html')),
+    url(r'^error/500/', *template_view(template='500.html')),
 )
 
 # vim:ts=4 sw=4 et
